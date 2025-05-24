b61806a834b32bf63d9e59541e9ca72bd05595f8
Marcelo Amaral Alves <marceloamaralalves@Marcelos-Mac-mini.local>
2025-05-24 02:11:32 -0300

    aula01

---
+++ swiftWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftWeatherApp
+//
+//  Created by Marcelo Amaral Alves on 24/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
